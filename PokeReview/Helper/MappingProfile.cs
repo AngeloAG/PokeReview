e9c8af61ce161df52c99396da8c6a3067309d58d
AngeloAG <angelo.ag.079@gmail.com>
2023-12-27 22:54:52 -0700

    Craete DTO and automapper profile

---
+++ PokeReview/Helper/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using PokeReview.Dto;
+using PokeReview.Models;
+
+namespace PokeReview.Helper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
