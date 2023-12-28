6d8f0497351061d2bfe61f06534ed4facb444c62
AngeloAG <angelo.ag.079@gmail.com>
2023-12-27 22:54:38 -0700

    Added more methods to the repo

---
+++ PokeReview/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokeReview.Models;
+
+namespace PokeReview.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int pokeId);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
+    }
+}
